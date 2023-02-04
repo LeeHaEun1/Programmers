328d8ee612f9749e14fef180291ba2134d06fe4f
Ha-eun, Lee <111097397+LeeHaEun1@users.noreply.github.com>
2023-02-05 02:25:21 +0900

    Add files via upload

---
+++ Level 1/부족한 금액 계산하기/RideFee.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Solution
+{
+    public long solution(int price, int money, int count)
+    {
+        long answer = 0;
+        
+        for(int i = 1; i <= count; i++)
+        {
+            answer += price * i;   
+        }
+        
+        if(answer >= money)
+        {
+            return(answer - money);
+        }
+        else
+        {
+            answer = 0;
+            return(answer);
+        }                          
+    }
+}
